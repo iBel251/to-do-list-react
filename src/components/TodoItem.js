0070c3569418f73b067537d3f78886106de1b1c0
iBel251 <ibbimran7@gmail.com>
2022-06-01 11:52:25 +0300

    Setup linters

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,14 @@
-import React from "react"
-import styles from "./TodoItem.module.css"
+import React from 'react';
+import styles from './TodoItem.module.css';
 
 const TodoItem = (props) => {
   const { completed, id, title } = props.todo;
   const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
+    fontStyle: 'italic',
+    color: '#595959',
     opacity: 0.4,
-    textDecoration: "line-through",
-  }
+    textDecoration: 'line-through',
+  };
   return (
     <li className={styles.item}>
       <input
@@ -17,14 +17,14 @@ const TodoItem = (props) => {
         checked={completed}
         onChange={() => props.handleChangeProps(id)}
       />
-      <button onClick={() => props.deleteTodoProps(id)}>
+      <button type="button" onClick={() => props.deleteTodoProps(id)}>
         Delete
       </button>
       <span style={completed ? completedStyle : null}>
         {title}
       </span>
     </li>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
